9eb4ca429de7550dda302af18e64bc6ed45376ce
admin <zhaoxumin@uni-ubi.com>
2019-07-11 13:55:12 +0800

    test demo

--- src/test/java/com/agile/go/AgileApplicationTests.java
+++ src/test/java/com/agile/go/AgileApplicationTests.java
@@ -4,13 +4,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class AgileApplicationTests {
 
-//    @Test
-//    public void contextLoads() {
-//    }
+    @Test
+    public void contextLoads() {
+    }
 
 }
